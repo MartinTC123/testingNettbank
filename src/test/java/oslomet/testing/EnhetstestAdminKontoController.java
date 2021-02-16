d8aac3f3ced30affadef3900a14db55d87bb0b8b
kykauwi <themagnus1208@gmail.com>
2021-02-16 16:10:31 +0100

    Testet hentAlleKonti i AdminKontoController, men den gir et nullPointerException jeg må se nærmere på

--- src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKontoController.java
@@ -3,26 +3,63 @@ package oslomet.testing;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import oslomet.testing.API.AdminKundeController;
+import oslomet.testing.API.AdminKontoController;
 import oslomet.testing.DAL.AdminRepository;
+import oslomet.testing.Models.Konto;
 import oslomet.testing.Sikkerhet.Sikkerhet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 
 public class EnhetstestAdminKontoController {
     @InjectMocks
-    private AdminKundeController kundeController;
+    //denne skal testes
+    private AdminKontoController adminKontoController;
 
     @Mock
+    //denne skal Mock'es
     private AdminRepository repository;
 
     @Mock
-    private Sikkerhet sikkerhet;
+    //denne skal Mock'es
+    private Sikkerhet sjekk;
 
     @Test
     public void hentAlleKonti_LoggetInn(){
+        //Arrange
+        List<Konto> konti = new ArrayList<>();
+        Konto konto1 = new Konto("105010123456", "01010110523",
+                720, "Lønnskonto", "NOK", null);
+        Konto konto2 = new Konto("105010123456", "12345678901",
+                1000, "Lønnskonto", "NOK", null);
+        konti.add(konto1);
+        konti.add(konto2);
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        when(repository.hentAlleKonti()).thenReturn(konti);
+
+        //act
+        List<Konto> resultat = adminKontoController.hentAlleKonti();
+
+        //assert
+        assertEquals(konti,resultat);
 
     }
     @Test
     public void hentAlleKonti_IkkeLoggetInn(){
+        //arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act
+        List<Konto> resultat =adminKontoController.hentAlleKonti();
+
+        assertNull(resultat);
 
     }
     @Test
